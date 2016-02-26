15c6b084cc383bd9d1f3a5f420b0f138247c354c
Justin Hugg <hugg@geneseo.edu>
2016-02-26 08:55:10 -0500

    parameterize url query

--- disabilityservice.py
+++ disabilityservice.py
@@ -6,9 +6,9 @@ import os
 from datetime import datetime
 from dateutil import parser
 
-date = datetime.now().strftime('%Y%m%d%H%M%S')
+def date = datetime.now().strftime('%Y%m%d%H%M%S')
 
-devprod = raw_input("[1]dev [2]prod [3]local: ")
+def devprod = raw_input("[1]dev [2]prod [3]local: ")
 
 def create_hash(date, shared_secret):
 		date_secret_unicode = unicode(date+shared_secret, "UTF-8")
@@ -18,17 +18,19 @@ def create_hash(date, shared_secret):
 		hash_str = m.hexdigest()
 		return hash_str
 
-username = raw_input('username? ')
+def username = raw_input('username? ')
+
+def search_params = '?timeStamp='+date+'&hash='+hash_str+'&username='+username
 
 if (username):
 		shared_secret = os.environ['SECRET']
 		hash_str = create_hash(date, shared_secret)
 		if (devprod == '1'):
-				url = os.environ['DEV_URL'] + '?timeStamp='+date+'&hash='+hash_str+'&username='+username
+				url = os.environ['DEV_URL'] + search_params
 		elif (devprod == '2'):
-				url = os.environ['PROD_URL'] + '?timeStamp='+date+'&hash='+hash_str+'&username='+username
+				url = os.environ['PROD_URL'] + search_params
 		elif (devprod == '3'):
-				url = url = os.environ['LOCAL_URL'] + '?timeStamp='+date+'&hash='+hash_str+'&username='+username
+				url = url = os.environ['LOCAL_URL'] + search_params
 else:
 		print "no username entered."
 
